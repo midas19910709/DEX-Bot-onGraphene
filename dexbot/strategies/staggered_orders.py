xeroc/stakemachine

commit 32dbafbcace9d1113578416cc24041d75eea3741

Fix race condition in SO
A rare race condition could occur when increasing an order and replacing
same order as partially filled.

dexbot/strategies/staggered_orders.py
@@ -710,7 +710,7 @@ class Strategy(StrategyBase):
         own_precision = 0
         opposite_precision = 0
         opposite_symbol = ''
-        increase_finished = False
+        increase_status = None
 
         if asset == 'base':
             order_type = 'buy'
@@ -869,16 +869,16 @@ class Strategy(StrategyBase):
                         # Lower/upper bound has been reached and now will start allocating rest of the balance.
                         self['bootstrapping'] = False
                         self.log.debug('Increasing sizes of {} orders'.format(order_type))
-                        increase_finished = self.increase_order_sizes(asset, asset_balance, own_orders)
+                        increase_status = self.increase_order_sizes(asset, asset_balance, own_orders)
                     else:
                         # Range bound is not reached, we need to add additional orders at the extremes
                         self['bootstrapping'] = False
                         self.log.debug('Placing further order than current furthest {} order'.format(order_type))
                         self.place_further_order(asset, furthest_own_order, allow_partial=True)
                 else:
-                    increase_finished = True
+                    increase_status = 'done'
 
-            if (increase_finished and not self.check_partial_fill(closest_own_order)
+            if (increase_status == 'done' and not self.check_partial_fill(closest_own_order)
                     and not self.check_partial_fill(closest_opposite_order, fill_threshold=0)):
                 """ Replace partially filled closest orders only when allocation of excess funds was finished. This
                     would prevent an abuse case when we are operating inactive market. An attacker can massively dump
@@ -895,8 +895,8 @@ class Strategy(StrategyBase):
                 # Refresh balances to make "reserved" funds available
                 self.refresh_balances(use_cached_orders=True)
                 self.replace_partially_filled_order(closest_own_order)
-            elif (increase_finished and not self.check_partial_fill(closest_opposite_order, fill_threshold=(
-                    1 - self.partial_fill_threshold)) and self.bitshares.txbuffer.is_empty()):
+            elif (increase_status == 'done' and not self.check_partial_fill(closest_opposite_order, fill_threshold=(
+                    1 - self.partial_fill_threshold))):
                 # Dust order on opposite side, cancel dust order and place closer order
                 # Require empty txbuffer to avoid rare condition when order may be already canceled from
                 # replace_partially_filled_order() call.
@@ -1310,9 +1310,11 @@ class Strategy(StrategyBase):
             :param str asset: 'base' or 'quote', depending if checking sell or buy
             :param Amount asset_balance: Balance of the account
             :param list orders: List of buy or sell orders
-            :return: True = all available funds were allocated
-                     False = not all funds were allocated, can increase more orders next time
-            :rtype: bool
+            :return: status of funds allocation
+                    done = all funds were allocated
+                    done_with_ops = all funds were allocated, operations are pending in txbuffer
+                    in_progress = not all funds were allocated, can increase more orders next time
+            :rtype: str
         """
 
         # Create temp order list (copy.deepcopy() doesn't work here)
@@ -1337,6 +1339,7 @@ class Strategy(StrategyBase):
         opposite_symbol = ''
         precision = 0
         opposite_precision = 0
+        ops_num = 0
 
         if asset == 'quote':
             order_type = 'sell'
@@ -1396,11 +1399,15 @@ class Strategy(StrategyBase):
                         self.place_market_buy_order(order['quote']['amount'], price)
 
                 # Limit number of operations to send at once
-                if len(self.bitshares.txbuffer.ops) > 10:
-                    return False
+                ops_num = len(self.bitshares.txbuffer.ops)
+                if ops_num > 10:
+                    return 'in_progress'
 
-        # All funds were used
-        return True
+        if ops_num < 10:
+            return 'done_with_ops'
+        else:
+            # All funds were used
+            return 'done'
 
     def check_partial_fill(self, order, fill_threshold=None):
         """ Checks whether order was partially filled it needs to be replaced

midas19910709/DEX-Bot-onGraphene

commit 47d6dda6b3c01f666aee0a97583a958cc508ef5d

Fix cancel dust order
A slight regression was introduced in 32dbafbcace9d1113578416cc24041d75eea3741

dexbot/strategies/staggered_orders.py
@@ -1403,11 +1403,11 @@ class Strategy(StrategyBase):
                 if ops_num > 10:
                     return 'in_progress'
 
-        if ops_num < 10:
+        if 0 < ops_num < 10:
             return 'done_with_ops'
-        else:
-            # All funds were used
-            return 'done'
+
+        # All funds were used
+        return 'done'
 
     def check_partial_fill(self, order, fill_threshold=None):
         """ Checks whether order was partially filled it needs to be replaced

tests/strategies/staggered_orders/test_staggered_orders_complex.py
@@ -969,7 +969,37 @@ def test_allocate_asset_dust_order_simple(worker, do_initial_allocation, maintai
     assert num_sell_orders_after - num_sell_orders_before == 1
 
 
-def test_allocate_asset_dust_order_increase(worker, do_initial_allocation, base_account, issue_asset):
+def test_allocate_asset_dust_order_excess_funds(
+    worker, do_initial_allocation, maintain_until_allocated, base_account, issue_asset
+):
+    """ Make dust order, add additional funds, these funds should be allocated
+        and then dust order should be canceled and closer opposite order placed
+    """
+    do_initial_allocation(worker, worker.mode)
+    num_sell_orders_before = len(worker.sell_orders)
+    num_buy_orders_before = len(worker.buy_orders)
+    additional_account = base_account()
+
+    # Partially fill order from another account
+    sell_price = worker.buy_orders[0]['price'] / 1.01
+    sell_amount = worker.buy_orders[0]['quote']['amount'] * (1 - worker.partial_fill_threshold) * 1.1
+    worker.market.sell(sell_price, sell_amount, account=additional_account)
+
+    # Add some balance to the worker
+    issue_asset(worker.market['quote']['symbol'], worker.sell_orders[0]['base']['amount'], worker.account.name)
+
+    worker.refresh_balances()
+    worker.refresh_orders()
+    worker.allocate_asset('quote', worker.quote_balance)
+    worker.refresh_orders()
+    num_sell_orders_after = len(worker.sell_orders)
+    num_buy_orders_after = len(worker.buy_orders)
+
+    assert num_buy_orders_before - num_buy_orders_after == 1
+    assert num_sell_orders_after - num_sell_orders_before == 1
+
+
+def test_allocate_asset_dust_order_increase_race(worker, do_initial_allocation, base_account, issue_asset):
     """ Test for https://github.com/Codaone/DEXBot/issues/587
 
         Check if cancelling dust orders on opposite side will not cause a race for allocate_asset() on opposite side
@@ -1021,9 +1051,7 @@ def test_allocate_asset_filled_orders(worker, do_initial_allocation, base_accoun
     assert num_sell_orders_after - num_sell_orders_before == 1
 
 
-def test_allocate_asset_filled_order_on_massively_imbalanced_sides(
-    worker, do_initial_allocation, base_account
-):
+def test_allocate_asset_filled_order_on_massively_imbalanced_sides(worker, do_initial_allocation, base_account):
     """ When sides are massively imbalanced, make sure that spread will be closed after filling one order on
         smaller side. The goal is to test a situation when one side has a big-sized orders, and other side has much
         smaller orders. Correct behavior: when order on smaller side filled, big side should place closer order.